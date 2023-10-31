cf46ab79f45a76645eb0153fa3f3ab517740da7a
juleni <69912179+juleni@users.noreply.github.com>
2023-10-31 11:34:32 +0100

    Implemented sendPost() - upload image to firestore

--- src/Preview.js
+++ src/Preview.js
@@ -7,10 +7,15 @@ import NoteIcon from "@mui/icons-material/Note";
 import SendIcon from "@mui/icons-material/Send";
 import TextFieldsIcon from "@mui/icons-material/TextFields";
 import TimerIcon from "@mui/icons-material/Timer";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore/lite";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 import { resetCameraImage, selectCameraImage } from "./features/cameraSlice";
+import { db, storage } from "./firebase";
+
 import "./Preview.css";
 
 function Preview() {
@@ -23,10 +28,38 @@ function Preview() {
   }, [cameraImage, navigate]);
 
   const closePreview = () => {
+    console.log("STORAGE=", storage);
     dispatch(resetCameraImage());
   };
 
-  const sendPost = () => {};
+  const sendPost = () => {
+    const id = uuid();
+    const storageRef = ref(storage, `posts/${id}`);
+
+    const uploadTask = uploadBytesResumable(storageRef, cameraImage);
+    uploadTask.on(
+      "state_changed",
+      null,
+      (error) => {
+        // ERROR
+        console.log(error);
+      },
+      () => {
+        // COMPLETE
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          // console.log("File available at", downloadURL);
+          setDoc(doc(db, "posts", "image"), {
+            imageUrl: downloadURL,
+            userName: "Juleni Dev",
+            read: false,
+            // profilePic,
+            timestamp: serverTimestamp(),
+          });
+          navigate("/chats", { replace: true });
+        });
+      }
+    );
+  };
 
   return (
     <div className="preview">
