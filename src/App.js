13d31c534bc70e105333743857c0b761d84a8b28
juleni <69912179+juleni@users.noreply.github.com>
2023-10-31 13:47:25 +0100

    Added Chats component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Chats from "./Chats";
 import Preview from "./Preview";
 import WebcamCapture from "./WebcamCapture";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="app">
       <Routes>
+        <Route path="/chats" element={<Chats />} />
         <Route path="/preview" element={<Preview />} />
         <Route path="/" element={<WebcamCapture />} />
       </Routes>
