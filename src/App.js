4cc2efdac83bd415094e7b87d690369255dbf4c1
juleni <69912179+juleni@users.noreply.github.com>
2023-11-07 16:30:07 +0100

    Added routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Chats from "./Chats";
+import ChatView from "./ChatView";
 import Preview from "./Preview";
 import WebcamCapture from "./WebcamCapture";
 
@@ -9,9 +10,10 @@ function App() {
   return (
     <div className="app">
       <Routes>
-        <Route path="/chats" element={<Chats />} />
-        <Route path="/preview" element={<Preview />} />
         <Route path="/" element={<WebcamCapture />} />
+        <Route path="/preview" element={<Preview />} />
+        <Route path="/chats" element={<Chats />} />
+        <Route path="/chats/view" element={<ChatView />} />
       </Routes>
     </div>
   );
