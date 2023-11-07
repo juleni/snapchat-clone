5dda2c2bec440253ea254b486751bce46f9253b2
juleni <69912179+juleni@users.noreply.github.com>
2023-11-07 16:29:56 +0100

    Added Chat component

---
+++ src/Chat.js
@@ -0,0 +1,43 @@
+import StopRoundedIcon from "@mui/icons-material/StopRounded";
+import { Avatar } from "@mui/material";
+import { doc, updateDoc } from "firebase/firestore";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import ReactTimeago from "react-timeago";
+import "./Chat.css";
+import { selectImage } from "./features/appSlice";
+import { db } from "./firebase";
+
+function Chat({ id, userName, timestamp, read, imageUrl, profilePic }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  console.log("--- Chat ---");
+
+  const open = async () => {
+    console.log("---OPEN read=", read);
+    console.log("---imageUrl=", imageUrl);
+    if (!read) {
+      dispatch(selectImage(imageUrl));
+      const docRef = doc(db, "posts", id);
+      await updateDoc(docRef, { read: true });
+      navigate("/chats/view", { replace: true });
+    }
+  };
+
+  return (
+    <div onClick={open} className="chat">
+      <Avatar className="chat__avatar" src={profilePic} />
+      <div className="chat__info">
+        <h4>{userName}</h4>
+        <p>
+          Tap to view -{" "}
+          <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()} />
+        </p>
+      </div>
+      {!read && <StopRoundedIcon className="chat__readIcon" />}
+    </div>
+  );
+}
+
+export default Chat;
