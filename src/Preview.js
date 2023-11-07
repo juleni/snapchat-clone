4f0d86f1e5aa5d7c2969ad026045b677f26ae943
juleni <69912179+juleni@users.noreply.github.com>
2023-11-07 16:28:04 +0100

    implemented uploading image

--- src/Preview.js
+++ src/Preview.js
@@ -1,3 +1,4 @@
+import { uploadString } from "@firebase/storage";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
 import CloseIcon from "@mui/icons-material/Close";
 import CreateIcon from "@mui/icons-material/Create";
@@ -7,8 +8,8 @@ import NoteIcon from "@mui/icons-material/Note";
 import SendIcon from "@mui/icons-material/Send";
 import TextFieldsIcon from "@mui/icons-material/TextFields";
 import TimerIcon from "@mui/icons-material/Timer";
-import { doc, serverTimestamp, setDoc } from "firebase/firestore/lite";
-import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { getDownloadURL, ref } from "firebase/storage";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -28,24 +29,53 @@ function Preview() {
   }, [cameraImage, navigate]);
 
   const closePreview = () => {
-    console.log("STORAGE=", storage);
     dispatch(resetCameraImage());
   };
 
-  const sendPost = () => {
+  const sendPost = async () => {
     const id = uuid();
-    const storageRef = ref(storage, `posts/${id}`);
-
-    const uploadTask = uploadBytesResumable(storageRef, cameraImage);
+    const imageRef = ref(storage, `posts/${id}`);
+    //const uploadTask = uploadBytesResumable(imageRef, cameraImage);
+    // console.log("cameraImage=", cameraImage);
+    const uploadTask = await uploadString(
+      imageRef,
+      cameraImage,
+      "data_url"
+    ).then((snapshot) => {
+      // console.log("uploadTask=", uploadTask);
+      // console.log("posts/Id=", `posts/${id}`);
+      // console.log("cameraImage=", cameraImage);
+      console.log("snapshot=", snapshot);
+      console.log("snapshot.ref=", snapshot.ref);
+      getDownloadURL(snapshot.ref).then((downloadURL) => {
+        // console.log("File available at", downloadURL);
+        setDoc(doc(db, "posts", "image"), {
+          imageUrl: downloadURL,
+          userName: "Juleni Dev",
+          read: false,
+          profilePic: "",
+          timestamp: serverTimestamp(),
+        });
+      });
+      navigate("/chats", { replace: true });
+    });
+    /*
     uploadTask.on(
       "state_changed",
-      null,
+      (snapshot) => {
+        console.log("snapshot=", snapshot);
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("upload progress is: " + progress);
+      },
       (error) => {
         // ERROR
         console.log(error);
       },
       () => {
         // COMPLETE
+        console.log("COMPLETE");
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           // console.log("File available at", downloadURL);
           setDoc(doc(db, "posts", "image"), {
@@ -59,6 +89,7 @@ function Preview() {
         });
       }
     );
+    */
   };
 
   return (
