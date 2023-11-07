16a1a7e702ddf5ff722ed1d49a65922c058851b5
juleni <69912179+juleni@users.noreply.github.com>
2023-11-07 16:31:30 +0100

    Changed setting up posts

--- src/Chats.js
+++ src/Chats.js
@@ -1,7 +1,8 @@
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 import SearchIcon from "@mui/icons-material/Search";
 import { Avatar } from "@mui/material";
-import { collection, getDocs, orderBy, query } from "firebase/firestore/lite";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+
 import React, { useEffect, useState } from "react";
 import Chat from "./Chat";
 import "./Chats.css";
@@ -11,30 +12,58 @@ function Chats() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
+    //console.log("--- Chats useEffect");
+    /**
     const getData = async () => {
       try {
+        //console.log("getdata --- q  = ", q);
         const querySnapshot = await getDocs(q);
-        setPosts(
-          querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            data: doc.data(),
-          }))
-        );
-        console.log("posts=", posts);
+        //console.log("querySnapshot=", querySnapshot);
+        //console.log("querySnapshot.docs=", querySnapshot.docs);
+
+        querySnapshot.forEach((doc) => {
+          console.log(doc.id, " => ", doc.data());
+          setPosts([
+            {
+              id: doc.id,
+              data: doc.data(),
+            },
+          ]);
+          console.log("INSIDE posts=", posts);
+        });
+        console.log("INSIDE posts1 =", posts);
+
+        //querySnapshot.docs.map((doc) => ())
 
         //querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
-        // console.log(doc.id, " => ", doc.data());
+        //console.log(doc.id, " => ", doc.data());
         //setPosts()
         //});
       } catch (error) {
         console.log("Error getting Documents", error);
       }
     };
-
+ */
     const postsRef = collection(db, "posts");
+    //console.log("postsRef=", postsRef);
     const q = query(postsRef, orderBy("timestamp", "desc"));
-    getData();
+    onSnapshot(q, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        //console.log("doc=", doc);
+        // console.log(doc.id, " => ", doc.data());
+        setPosts([
+          {
+            id: doc.id,
+            data: doc.data(),
+          },
+        ]);
+      });
+      //console.log("INSIDE posts=", posts);
+    });
+
+    //getData();
+    //console.log("posts=", posts);
   }, []);
 
   return (
@@ -48,17 +77,22 @@ function Chats() {
         <ChatBubbleIcon className="chats__chatIcon" />
       </div>
       <div className="chats__posts">
-        {posts.map(({ id, data: { imageUrl, read, timestamp, userName, profilePic } }) => (
-          <Chat
-            key={id}
-            id={id}
-            userName={userName}
-            imageUrl={imageUrl}
-            read={read}
-            timestamp={timestamp}
-            profilePic={profilePic}
-          />
-        ))}
+        {posts?.map(
+          ({
+            id,
+            data: { imageUrl, read, timestamp, userName, profilePic },
+          }) => (
+            <Chat
+              key={id}
+              id={id}
+              userName={userName}
+              imageUrl={imageUrl}
+              read={read}
+              timestamp={timestamp}
+              profilePic={profilePic}
+            />
+          )
+        )}
       </div>
     </div>
   );
