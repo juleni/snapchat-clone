c995fe49623529bf2cc01d3c154f4e0bf517319c
juleni <69912179+juleni@users.noreply.github.com>
2023-11-09 13:15:35 +0100

    Added login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Chats from "./Chats";
 import ChatView from "./ChatView";
+import { selectUser } from "./features/appSlice";
+import Login from "./Login";
 import Preview from "./Preview";
 import WebcamCapture from "./WebcamCapture";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
   return (
     <div className="app">
-      <Routes>
-        <Route path="/" element={<WebcamCapture />} />
-        <Route path="/preview" element={<Preview />} />
-        <Route path="/chats" element={<Chats />} />
-        <Route path="/chats/view" element={<ChatView />} />
-      </Routes>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app_body">
+          <Routes>
+            <Route path="/" element={<WebcamCapture />} />
+            <Route path="/preview" element={<Preview />} />
+            <Route path="/chats" element={<Chats />} />
+            <Route path="/chats/view" element={<ChatView />} />
+          </Routes>
+        </div>
+      )}
     </div>
   );
 }
