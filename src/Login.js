0f61aedebe8b93c9724d1eedeb1d5b6f2f22149c
juleni <69912179+juleni@users.noreply.github.com>
2023-11-09 14:24:41 +0100

    Implemented signIn() method

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,33 @@
 import { Button } from "@mui/material";
+import { signInWithPopup } from "firebase/auth";
 import React from "react";
 import { useDispatch } from "react-redux";
+import { login } from "./features/appSlice";
+import { auth, provider } from "./firebase";
 import "./Login.css";
 
 function Login() {
   const dispatch = useDispatch();
-  const signIn = () => {};
+  const signIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log("result=", result);
+        dispatch(
+          login({
+            username: result.user.displayName,
+            profilePic: result.user.photoUrl,
+            id: result.user.id,
+          })
+        );
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <div className="login">
       <div className="login__container">
         <img src="snapchat.png" alt="" />
-        <Button variant="outline" onClick={signIn}>
+        <Button variant="outlined" onClick={signIn}>
           Sign In
         </Button>
       </div>
