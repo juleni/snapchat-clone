187b8bbffdce13c1f9cd8cd406447c70aec6594f
juleni <69912179+juleni@users.noreply.github.com>
2023-11-09 15:47:22 +0100

    Added useEffect and login impl

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Chats from "./Chats";
 import ChatView from "./ChatView";
-import { selectUser } from "./features/appSlice";
+import { login, logout, selectUser } from "./features/appSlice";
+import { auth } from "./firebase";
 import Login from "./Login";
 import Preview from "./Preview";
 import WebcamCapture from "./WebcamCapture";
@@ -12,6 +14,25 @@ import WebcamCapture from "./WebcamCapture";
 function App() {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (authUser) => {
+      if (authUser) {
+        // User is signed in
+        dispatch(
+          login({
+            username: authUser.displayName,
+            profilePic: authUser.photoURL,
+            id: authUser.uid,
+          })
+        );
+      } else {
+        // User is signed out
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       {!user ? (
